a7a3d11bad99bd71c04f6960ff3e59b3b9c6cb41
dino soifua <dinopearson801@gmail.com>
2018-07-06 08:29:09 -0600

    built first action creator and built courses reducer and connected library component to redux

--- src/components/library/library.js
+++ src/components/library/library.js
@@ -1,20 +1,26 @@
 import React, { Component } from 'react';
+import { connect } from 'react-redux';
+
+import * as actions from '../../actions';
+
 import LibraryCourse from './libraryCourse';
 
-
-
 class Library extends Component {
-    render(){
-        return(
-            <div className='library'>
+
+    componentDidMount() {
+        this.props.fetchCourses()
+    }
+
+    render() {
+        return (
+            <div className="library">
                 <h1 className="library__title">Course Library</h1>
                 <LibraryCourse/>
                 <LibraryCourse/>
                 <LibraryCourse/>
-
             </div>
         )
     }
 }
 
-export default Library;
+export default connect(null, actions)(Library);
