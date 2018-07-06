c4a75c8516b49cb799c16b8906467473322c6c17
dino soifua <dinopearson801@gmail.com>
2018-07-05 20:30:24 -0600

    built first action creator

---
+++ src/actions/index.js
@@ -0,0 +1,19 @@
+imort {FETCH_COURSE} from '/types';
+
+function fetchCourses(){
+    retur{
+        action:FETCH_COURSES,
+        payload:[
+            {
+                title:"Problem Solving",
+                description: " "
+            }
+            {
+                title:
+            }
+            {
+                title:
+            }
+        ]
+    }
+}
